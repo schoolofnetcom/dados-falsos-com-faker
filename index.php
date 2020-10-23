66469428201e6a28ecf57fce83baad77dd43afc4
thivalls <olavalls@gmail.com>
2020-10-23 14:17:19 -0300

    Subindo arquivos do direto ao ponto

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+    require __DIR__ . "/vendor/autoload.php";
+
+    $faker = Faker\Factory::create('pt_BR');
+
+    // echo $faker->randomDigit;
+    // echo $faker->randomDigitNot(2);
+    // echo $faker->randomNumber(3, true);
+    // echo $faker->randomFloat(2, 10, 20);
+    // echo $faker->numberBetween(10, 20);
+    // echo $faker->randomLetter;
+    // var_dump($faker->randomElements(['a', 'b', 'c'], 2));
+    // echo $faker->randomElement(['a', 'b', 'c', 'd']);
+    // echo $faker->numerify('Número ###');
+    // echo $faker->numerify('(##) #####-####');
+    // echo $faker->lexify('teste ????');
+    // echo $faker->bothify('##??##??');
+
+    // echo $faker->word;
+    // echo $faker->words(4, true);
+    // echo $faker->sentence(4, true);
+    // echo $faker->sentences(4, true);
+    // echo $faker->paragraph();
+    // echo $faker->paragraphs(2, true);
+    // echo $faker->text(1000);
+
+    // PESSOAS
+    // echo $faker->name();
+    // echo $faker->name('male');
+    // echo $faker->name('female');
+    // echo $faker->firstname(); // male / female
+
+    // echo $faker->state;
+    //echo $faker->stateAbbr;
+    // echo $faker->region; // pt_BR
+    // echo $faker->regionAbbr; // pt_BR
+
+    // echo $faker->city;
+    // echo $faker->streetName;
+    // echo $faker->streetAddress;
+    // echo $faker->buildingNumber;
+    // echo $faker->address;
+    // echo $faker->country;
+    // echo $faker->latitude;
+    // echo $faker->longitude(-90, 90);
+
+    // echo $faker->date('d-m-Y');
+    // echo $faker->time();
+    // echo $faker->amPm();
+    // echo $faker->month;
+    // echo $faker->monthName();
+    // echo $faker->year();
+
+    //echo $faker->email;
+    // echo $faker->password();
+    // echo $faker->url;
+    // echo $faker->slug();
+
+    // echo $faker->creditCardType;
+    // echo $faker->creditCardNumber();
+    // var_dump($faker->creditCardExpirationDate());
+    // echo $faker->creditCardExpirationDateString();
+
+    // echo $faker->colorName;
+    // echo $faker->hexColor;
+    // echo $faker->rgbColor;
+
+    // echo $faker->imageUrl(100, 100);
+
+    // echo $faker->uuid;
+    // echo $faker->boolean();
+    // echo $faker->currencyCode();
+    // echo $faker->emoji;
+
+    // echo $faker->cpf;
+    // echo $faker->cpf(false);
+    //echo $faker->cnpj;
+    // echo $faker->cnpj(false);
+
+    // echo $faker->phone;
+    // echo $faker->cellphone;
+    // echo $faker->phoneNumber;
+    // echo $faker->areaCode;
+    
+
+
+
+    
